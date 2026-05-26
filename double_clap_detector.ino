bd79ca84b5cca7f3fe02cc4a5a05deb1e89867b9
ofrico <79369916+ofrico@users.noreply.github.com>
2026-05-26 22:35:28 +0300

    Update double_clap_detector.ino

--- double_clap_detector.ino
+++ double_clap_detector.ino
@@ -5,30 +5,25 @@
 
   26/5/2026
 
-  Connect DO of your sound sensor to pin 2 of your microcontroller,
-  connect a LED using a 220 ohm resistor to pin 3.
+  Connect DO of your sound sensor to pin 2 of your microcontroller
 
-  Make shure to calibrate your sound sensor.
+  Make shure to calibrate your sound sensor
 */
 
 bool IsDoubleClap = LOW;
+unsigned long Time;
 
 void setup(){
   pinMode(2, INPUT);
-  pinMode(3, OUTPUT);
 }
 
 void loop() {
   if(digitalRead(2)==1){
     delay(100);
-    unsigned long startTime = millis();
-    while(digitalRead(2)==0){}
-    unsigned long Time = millis() - startTime;
-    if(Time<400) IsDoubleClap=HIGH; 
+    Time = pulseIn(2, HIGH, 500000);
+    if(Time<500000 && Time!=0){
+      IsDoubleClap = HIGH;
+      //Put here what you want to do when there is a double clap
+    } 
   }
-  
-  digitalWrite(3, IsDoubleClap);
-  delay(1000);
-  IsDoubleClap = LOW;
-  digitalWrite(3, LOW);
 }
